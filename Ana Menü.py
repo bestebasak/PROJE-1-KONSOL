b2e77b86d11f16c3477d0b602d44f9e034572fbd
bestebasak <bbbestee1905@gmail.com>
2024-07-31 19:24:28 +0300

    a

---
+++ Ana Menü.py
@@ -0,0 +1,19 @@
+import oyun.Oyunlar as oo
+
+def anamenu():
+    print("╔══════════════════*══════════════════╗")
+    print("║               ANA MENÜ              ║")
+    print("║                                     ║")
+    print("║ 1-Hesap Makinesi                    ║")
+    print("║ 2-Spor Türleri Hakkında Bilgi       ║")
+    print("║ 3-Not Ortalaması                    ║")
+    print("║ 4-Yaş Hesabı                        ║")
+    print("║ 5-Boy Kilo Endeksi                  ║")
+    print("║ 6-Sıcaklık Çevirme                  ║")
+    print("║ 7-Oyunlar                           ║")
+    print("║ 7-ÇIKIŞ                             ║")
+    print("║                                     ║")
+    print("║             SEÇİMİNİ YAP            ║")
+    print("╚═════════════════════════════════════╝")
+
+    input()
