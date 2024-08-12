b9a1af5092ef304a04b58ec55dc6994c83d292ec
bestebasak <bbbestee1905@gmail.com>
2024-08-12 20:50:12 +0300

    a

--- yasHesabi.py
+++ yasHesabi.py
@@ -2,5 +2,5 @@ def yasmenu():
     ad=input("Adınız: ")
     soyad=input("Soyadınız: ")
     doğumyılı=int(input(f"{ad} {soyad} doğum yılın ne? "))
-    print(f"{ad}, {2024-doğumyılı} yaşındasın.")
+    print(f"{ad} {soyad}, {2024-doğumyılı} yaşındasın.")
 
